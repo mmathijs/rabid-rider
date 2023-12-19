5609143f855197f9516467f82420f964f6c19c0f
Mathijs Markvoort <75422017+Gamermathijs@users.noreply.github.com>
2023-12-19 11:33:50 +0100

    Simple first test

---
+++ src/test/kotlin/GeometryTest.kt
@@ -0,0 +1,19 @@
+package nl.mmathijs.roadrunneraddons
+
+import com.acmerobotics.roadrunner.geometry.Vector2d
+import org.junit.jupiter.api.Test
+import kotlin.math.PI
+import kotlin.math.sqrt
+import kotlin.test.assertEquals
+
+class GeometryTest {
+
+    @Test
+    fun testLine2d() {
+        val line = Line2d(Vector2d(0.0, 0.0), Vector2d(1.0, 1.0))
+
+        assertEquals(sqrt(2.0), line.getLength())
+        assertEquals(Vector2d(0.5, 0.5), line.getMidpoint())
+        assertEquals(PI / 4, line.getAngle())
+    }
+}
